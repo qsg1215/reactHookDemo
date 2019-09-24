ca1ff6b49c0324edf49d58004076353ec1bad04b
huaqiao.chen <huaqiao.chen@biz-united.com.cn>
2019-09-24 17:00:53 +0800

    加入百度地图

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,22 +1,27 @@
 import React, { Component } from "react";
-
-class A {
-}
-
-class B extends A {
-}
+import './home.css'
+import { data } from './dataBase/road1.js'
+const { BMap } = window;
 
 
-
-export default class Home extends React.Component {
+export default class Home extends Component {
   componentDidMount() {
+    let BmapK = new BMap.Map("map", { enableMapClick: false });
+    BmapK.centerAndZoom(new BMap.Point(103.26462, 29.762742), 13);
+    BmapK.enableScrollWheelZoom(false);
 
 
+    let pointList = [];
+    pointList = data.map(item => new BMap.Point(item.longitude, item.latitude))
+    var polyline = new BMap.Polyline(pointList,
+      { strokeColor: "blue", strokeWeight: 6, strokeOpacity: 0.5 }
+    );
+    BmapK.addOverlay(polyline);
   }
   render() {
-    return <div>
-      测试案例
-  </div>
+    return <div id="map">
+      测试案例1234
+      </div>
   }
 
 
