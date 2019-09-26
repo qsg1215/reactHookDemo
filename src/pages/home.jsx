b7016e1e13568da5cff5d1e80e5d98d821bef3d0
huaqiao.chen <huaqiao.chen@biz-united.com.cn>
2019-09-26 09:25:48 +0800

    some file

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -4,31 +4,55 @@ import './home.css'
 
 
 export default class Home extends Component {
+  spawn(genF) {
+    return new Promise(function (resolve, reject) {
+      const gen = genF();
+      //gen 迭代器
+      function step(nextF) {
+
+        let next;
+        try {
+          next = nextF();
+        } catch (e) {
+          return reject(e);
+        }
+        console.log(next)
+        if (next.done) {
+          return resolve(next.value);
+        } else {
+          Promise.resolve(next.value).then(function (v) {
+            step(function () { return gen.next(v); });
+          }, function (e) {
+            step(function () { return gen.throw(e); });
+          });
+        }
+
+      }
+      step(function () { return gen.next(); });
+    });
+  }
   componentDidMount() {
-    class Sleep {
-      constructor(timeout) {
-        this.timeout = timeout;
-      }
-      then(resolve, reject) {
-        const startTime = Date.now();
-        setTimeout(
-          () => resolve(Date.now() - startTime),
-          this.timeout
-        );
-      }
+    let ginerateFunc = function* () {
+      yield new Promise((reslove, reject) => { setTimeout(() => { reslove('1') }, 3000) })
+      yield new Promise((reslove, reject) => { setTimeout(() => { reslove('2') }, 3000) });
+      return 12
     }
 
-    (async () => {
-      const sleepTime = await new Sleep(3000);
-      console.log(sleepTime);
-    })();
+    let beginTime = +(new Date)
+
+    this.spawn(ginerateFunc).then(res => {
+      let endTime = +(new Date)
+      console.log(endTime - beginTime)
+      console.log(res)
+    })
+
+
+
   }
   render() {
-    return <div id="map" onClick={event => { console.log(event) }}    >
-
-    <input onChange={event => { console.log(event) }}  />
-      测试案例1234
-      </div>
+    return <div id="map"   >
+      123
+    </div>
   }
 
 
