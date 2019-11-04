24f76f596387376fa532d6a9effaa6dcdd6fe96d
huaqiao.chen <huaqiao.chen@biz-united.com.cn>
2019-11-04 09:42:02 +0800

    file

--- src/pages/warppedComponent/index.jsx
+++ src/pages/warppedComponent/index.jsx
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
-import wrapWithLoadData from './components/wrapWithLoadData'
+// import wrapWithLoadData from './components/wrapWithLoadData'
 
-class InputWithUserName extends Component {
+// class InputWithUserName extends Component {
+//   render() {
+//     return <input value={this.props.data} />
+//   }
+// }
+
+// InputWithUserName = wrapWithLoadData(InputWithUserName, 'username')
+// export default InputWithUserName
+
+export default class Index extends Component{
   render() {
-    return <input value={this.props.data} />
-  }
+   return (<div>hello</div>)
+ }
 }
-
-InputWithUserName = wrapWithLoadData(InputWithUserName, 'username')
-export default InputWithUserName
