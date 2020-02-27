6904f432ccdfbd097523cd44c086323c10ddd6d7
huaqiao.chen <1184053575@qq.com>
2020-02-27 16:53:21 +0800

    file

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ ReactDOM.render(<AppRouter />, rootElement);
 
 let appState = {
     title: {
-        text: 'React.js 小书',
+        text: 'React.js 小书1',
         color: 'red',
     },
     content: {
